dc94db1fe467d74d8455a4c6f81cae7ceefe0d96
Anna Foldvari <annafoldvari@riverford.co.uk>
2018-08-21 14:46:03 +0100

    Empty fields feature added

--- app.js
+++ app.js
@@ -119,6 +119,22 @@ var UIController = (function() {
 
         },
 
+        clearFields: function() {
+
+          var fields, fieldsArr;
+
+          fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+          fieldsArr = Array.prototype.slice.call(fields);
+
+          fieldsArr.forEach(function(current, index, array) {
+              current.value = "";
+          });
+
+          fieldsArr[0].focus();
+
+        },
+
         getDOMstrings: function() {
           return  DOMstrings;
         }
@@ -157,9 +173,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
       UICtrl.addListItem(newItem, input.type);
 
-      // 4. Calculate the budget
+      // 4. Clear the fields
 
-      // 5. Display the budget on the UI
+      UICtrl.clearFields();
+
+      // 5. Calculate the budget
+
+      // 6. Display the budget on the UI
 
   };
 
