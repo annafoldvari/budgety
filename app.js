ba224505a08248b99f22493f9e17609b9db4ae91
Anna Foldvari <annafoldvari@riverford.co.uk>
2018-08-22 15:19:53 +0100

    Current date is added to budget title.

--- app.js
+++ app.js
@@ -165,7 +165,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
 
     };
 
@@ -294,6 +295,19 @@ var UIController = (function() {
             });
         },
 
+        displayMonth: function() {
+            var now, year, months, month;
+
+            now = new Date();
+
+
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+            year = now.getFullYear();
+
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+
         getDOMstrings: function() {
           return  DOMstrings;
         }
@@ -416,6 +430,7 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
       init: function() {
           console.log('Application has started.');
+          UIController.displayMonth();
           UICtrl.displayBudget({
               budget: 0,
               totalInc: 0,
